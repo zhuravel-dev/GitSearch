e749df23f7508bd63c8d257af525f6fb2f92eb4d
Yuliya <yz@rollncode.com>
2021-12-10 12:38:15 +0300

    optimaze UI: get tags, star symbols, stars count, owners login, date updated

--- app/src/main/java/com/example/gitsearch/ui/main/adapter/MainAdapter.kt
+++ app/src/main/java/com/example/gitsearch/ui/main/adapter/MainAdapter.kt
@@ -1,25 +1,44 @@
 package com.example.gitsearch.ui.main.adapter
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.core.text.HtmlCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.example.gitsearch.R
 import com.example.gitsearch.data.model.Item
 import com.example.gitsearch.databinding.ItemLayoutBinding
 import com.example.gitsearch.ui.main.extensions.viewBindingVH
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class MainAdapter(
     private val repository: ArrayList<Item>
 ) : RecyclerView.Adapter<MainAdapter.DataViewHolder>() {
 
-    class DataViewHolder(private val itemViewBinding: ItemLayoutBinding) : RecyclerView.ViewHolder(itemViewBinding.root) {
+    class DataViewHolder(private val itemViewBinding: ItemLayoutBinding) :
+        RecyclerView.ViewHolder(itemViewBinding.root) {
+
+        @SuppressLint("SetTextI18n")
+        @RequiresApi(Build.VERSION_CODES.O)
         fun bind(repository: Item) = itemViewBinding.run {
+            tvOwnerName.text = repository.owner.login.plus("/")
             tvRepositoryName.text = repository.name
             tvRepositoryDescription.text = repository.description
             tvProgramLanguage.text = repository.language
+            tvTags.text =
+                repository.topics.toString().substring(1, repository.topics.toString().length - 1);
+            tvStarCount.text = "\u2606 ${repository.stargazers_count}"
+            val updatedDate = repository.updated_at
+            val parsedDate = LocalDateTime.parse(updatedDate, DateTimeFormatter.ISO_DATE_TIME)
+            val formattedDate = parsedDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+            tvUpdatedAt.text = "Updated on $formattedDate"
         }
     }
-
     override fun getItemCount(): Int = repository.size
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: DataViewHolder, position: Int) =
         holder.bind(repository[position])
 
