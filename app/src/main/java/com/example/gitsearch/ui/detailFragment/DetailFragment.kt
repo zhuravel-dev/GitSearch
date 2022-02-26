1625c0d79564bf57237890d58d7652968ee815a1
Yuliya <yz@rollncode.com>
2022-02-26 22:00:58 +0200

    Detail Fragment by Jetpack Compose

--- app/src/main/java/com/example/gitsearch/ui/detailFragment/DetailFragment.kt
+++ app/src/main/java/com/example/gitsearch/ui/detailFragment/DetailFragment.kt
@@ -1,33 +1,53 @@
+/*
 package com.example.gitsearch.ui.detailFragment
 
-import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.paging.ExperimentalPagingApi
 import com.example.gitsearch.R
-import com.example.gitsearch.data.local.model.ItemLocalModel
-import com.example.gitsearch.data.local.model.OwnerLocalModel
-import com.example.gitsearch.databinding.FragmentDetailBinding
-import com.example.gitsearch.ui.extensions.viewBinding
-import com.squareup.picasso.Picasso
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import androidx.compose.ui.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.gitsearch.data.local.model.ItemLocalModel
+import com.example.gitsearch.data.local.model.OwnerLocalModel
+import com.example.gitsearch.ui.compose.DEFAULT_AVATAR_IMAGE
+import com.example.gitsearch.ui.compose.loadPicture
 
+
+const val IMAGE_HEIGHT = 260
+
+@OptIn(ExperimentalMaterialApi::class)
 @ExperimentalPagingApi
 @ExperimentalCoroutinesApi
 @AndroidEntryPoint
-class DetailFragment : Fragment(R.layout.fragment_detail) {
+class DetailFragment : Fragment() {
 
-    private val viewBinding: FragmentDetailBinding? by viewBinding(FragmentDetailBinding::bind)
     private val detailFragmentViewModel: DetailViewModel by viewModels()
     private val args: DetailFragmentArgs by navArgs()
     private val modelId by lazy { args.myModelId }
@@ -35,55 +55,171 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+*/
+/*lifecycleScope.launch {
+            detailFragmentViewModel.onIntent(DetailFragmentIntent.GetAllById(modelId, ownerId))
+        }*//*
 
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                detailFragmentViewModel.onIntent(DetailFragmentIntent.GetAllById(modelId, ownerId))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                val data = detailFragmentViewModel.onIntent(
+                    DetailFragmentIntent.GetAllById(
+                        modelId,
+                        ownerId
+                    )
+                )
+
+                */
+/*Box(modifier = Modifier.fillMaxSize()) {
+                    if (data == null) LoadingRecipeShimmer(imageHeight = IMAGE_HEIGHT.dp)
+                    else data.let {
+                        MyDetailScreen(model = it, ownerModel = it)
+                    }
+                }*//*
+
             }
         }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        setupToolbar()
-        observeViewModel()
-    }
-
-    private fun setupToolbar() = viewBinding?.run {
-        toolbar.customToolbar.setNavigationOnClickListener {
-            findNavController().popBackStack()
-        }
-    }
-
-    private fun observeViewModel() {
-        lifecycleScope.launch {
-                detailFragmentViewModel.state.collect {
-                    when (it) {
-                        is DetailFragmentState.Idle -> {}
-                        is DetailFragmentState.Loading -> {
-                            viewBinding?.progressBar?.visibility = View.VISIBLE
-                        }
-                        is DetailFragmentState.DataLoadedAll -> {
-                            viewBinding?.progressBar?.visibility = View.GONE
-                            showModelInformation(it.model, it.ownerModel)
-                        }
-                        is DetailFragmentState.Error -> {
-                            viewBinding?.progressBar?.visibility = View.GONE
+    @OptIn(ExperimentalFoundationApi::class)
+    @Composable
+    fun MyDetailScreen(
+        model: ItemLocalModel,
+        ownerModel: OwnerLocalModel
+    ) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text(text = "Detail info") },
+                    backgroundColor = MaterialTheme.colors.primary,
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            Icon(Icons.Default.Back, "Back")
+ }) {
+                            Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Back")
                         }
                     }
+                )
+            }
+        ) {
+            LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                stickyHeader {
+                    Box(
+                        Modifier
+                            .background(Color.Gray)
+                            .fillMaxWidth()
+                            .height(300.dp)
+                    )
+                    val image = loadPicture(
+                        url = ownerModel.avatar_url,
+                        defaultImage = DEFAULT_AVATAR_IMAGE
+                    ).value
+                    image?.let { img ->
+                        Image(
+                            bitmap = img.asImageBitmap(),
+                            contentDescription = "Owner`s avatar",
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(IMAGE_HEIGHT.dp),
+                            contentScale = ContentScale.Crop
+                        )
+                    }
+                }
+                item {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(bottom = 4.dp)
+                        ) {
+                            Text(
+                                text = ownerModel.login,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .wrapContentWidth(Alignment.Start),
+                                style = MaterialTheme.typography.h3
+                            )
+                            val nameOfRepository = "${model.name} repository"
+                            Text(
+                                text = nameOfRepository,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .wrapContentWidth(Alignment.Start),
+                                style = MaterialTheme.typography.h3
+                            )
+                            model.description?.let { description ->
+                                Text(
+                                    text = description,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .wrapContentWidth(Alignment.Start),
+                                    style = MaterialTheme.typography.h3
+                                )
+                                model.language?.let { language ->
+                                    Text(
+                                        text = language,
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .wrapContentWidth(Alignment.Start),
+                                        style = MaterialTheme.typography.h3
+                                    )
+                                }
+                                val topics = model.topics.toString()
+                                    .substring(1, model.topics.toString().length - 1)
+                                Text(
+                                    text = topics,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .wrapContentWidth(Alignment.Start),
+                                    style = MaterialTheme.typography.h3
+                                )
+                                val watchers = "${model.watchers.toString()} watchers"
+                                Text(
+                                    text = watchers,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .wrapContentWidth(Alignment.Start),
+                                    style = MaterialTheme.typography.h3
+                                )
+                            }
+                        }
+                    }
+                }
             }
         }
     }
-
-    @SuppressLint("SetTextI18n")
-    private fun showModelInformation(model: ItemLocalModel, ownerModel: OwnerLocalModel) = viewBinding?.run {
-        Picasso.get().load(ownerModel.avatar_url).into(ivUserAvatarDetailScreen)
-        tvUserLogin.text = ownerModel.login
-                tvNameOfRepository.text = "${model.name} repository"
-        tvRepositoryDescription.text = model.description
-        tvProgramingLanguages.text = model.language
-        tvTopics.text =
-            model.topics.toString().substring(1, model.topics.toString().length - 1)
-        tvWatchers.text = "${model.watchers.toString()} watchers"
-    }
 }
+
+
+*/
+/*private fun observeViewModel() {
+        lifecycleScope.launch {
+            detailFragmentViewModel.state.collect {
+                when (it) {
+                    is DetailFragmentState.Idle -> {}
+                    is DetailFragmentState.Loading -> {
+                        viewBinding?.progressBar?.visibility = View.VISIBLE
+                    }
+                    is DetailFragmentState.DataLoadedAll -> {
+                        viewBinding?.progressBar?.visibility = View.GONE
+                        showModelInformation(it.model, it.ownerModel)
+                    }
+                    is DetailFragmentState.Error -> {
+                        viewBinding?.progressBar?.visibility = View.GONE
+                    }
+                }
+            }
+        }
+    }
+ */
